f6376f872b809053525bb3de8f6300bbeaf85c47
Thameera Senanayaka <thameera123@gmail.com>
2017-11-03 22:54:11 +0530

    URL parser

--- views/parser.js
+++ views/parser.js
@@ -1,5 +1,7 @@
 ;(function($) {
 
+  const urlParser = require('./lib/url-parser')
+
   const ID = 'PARSER-PLUGIN'
 
   class Parser {
@@ -14,10 +16,44 @@
 
     setup() {
       this.$text = $('<textarea rows="6" class="tab-focus"></textarea>')
+      this.$text.bind('input propertychange', this.start.bind(this))
+
       this.$results = $('<div class="parser-results">res</div>')
 
       this.$el.append(this.$text)
       this.$el.append(this.$results)
+    }
+
+    start() {
+      const text = this.$text.val().trim()
+      const urlFields = urlParser(text)
+      if (urlFields) {
+        return this.showUrl(urlFields)
+      }
+    }
+
+    replaceResult($result) {
+      this.$results.empty()
+      this.$results.append($result)
+    }
+
+    showUrl(urlFields) {
+      const $table = $('<table class="url"></table>')
+      urlFields.forEach(f => {
+        const $tr = $('<tr>')
+        if (f.heading) {
+          const $td = (`<td class="heading">${f.heading}</td>`)
+          $tr.append($td)
+          console.log('heading')
+        } else {
+          const $td1 = (`<td>${f.name}</td>`)
+          const $td2 = (`<td class="val">${f.val}</td>`)
+          $tr.append($td1)
+          $tr.append($td2)
+        }
+        $table.append($tr)
+      })
+      this.replaceResult($table)
     }
   }
 
