736a78b7d0b6d5f07962732cda619af7eb2901f8
Thameera Senanayaka <thameera123@gmail.com>
2017-11-03 18:51:13 +0530

    Parser basic setup

--- views/parser.js
+++ views/parser.js
@@ -1,10 +1,34 @@
 ;(function($) {
 
+  const ID = 'PARSER-PLUGIN'
+
+  class Parser {
+    constructor(element) {
+      this.el = element
+      this.$el = $(element)
+      this.$text = null
+      this.$results = null
+
+      this.setup()
+    }
+
+    setup() {
+      this.$text = $('<textarea rows="6" class="tab-focus"></textarea>')
+      this.$results = $('<div class="parser-results">res</div>')
+
+      this.$el.append(this.$text)
+      this.$el.append(this.$results)
+    }
+  }
+
   $.fn.parser = function() {
 
-    this.each( function() {
-      $(this).text("Parser")
-    });
+    return this.each(function() {
+      const parser = $(this).data(ID)
+      if (!parser) {
+        $(this).data(ID, new Parser(this))
+      }
+    })
 
   }
 
