183e3f0c5041a840d5d705efeda1d3268d8503a7
Thameera Senanayaka <me@thameera.com>
2022-10-23 18:03:31 +1100

    Implement tabs

--- src/renderer.js
+++ src/renderer.js
@@ -3,8 +3,6 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-import './view/index.css'
-
 import './view/app.jsx'
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
--- src/view/app.jsx
+++ src/view/app.jsx
@@ -1,13 +1,53 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Box, CssBaseline, Tab, Tabs, ThemeProvider } from '@mui/material'
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import theme from './theme'
 
 const App = () => {
+  const [tabValue, setTabValue] = React.useState(0)
+
+  /*
+   * Show each tab panel only when it is active.
+   */
+  const getStyles = (tabId) => {
+    return {
+      display: tabValue === tabId ? 'block' : 'none',
+    }
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <h2>Hello!</h2>
+
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs className="tabs" value={tabValue} onChange={(_, newValue) => setTabValue(newValue)}>
+            <Tab label="Parser" id="tab-parser" />
+            <Tab label="Converter" id="tab-converter" />
+            <Tab label="Diff" id="tab-diff" />
+            <Tab label="Reference" id="tab-ref" />
+            <Tab sx={{ position: 'absolute', right: 0 }} label="About" id="tab-about" />
+          </Tabs>
+        </Box>
+
+        <Box sx={{ m: '5px' }}>
+          <Box sx={getStyles(0)}>
+            TODO: Parser
+          </Box>
+          <Box sx={getStyles(1)}>
+            TODO: Converter
+          </Box>
+          <Box sx={getStyles(2)}>
+            TODO: Diff
+          </Box>
+          <Box sx={getStyles(3)}>
+            TODO: Reference
+          </Box>
+          <Box sx={getStyles(4)}>
+            TODO: About
+          </Box>
+        </Box>
+      </Box>
     </ThemeProvider>
   )
 }
