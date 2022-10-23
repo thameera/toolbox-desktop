7aad684ed716d4176bc1cc290b8cc8ed27928efb
Thameera Senanayaka <me@thameera.com>
2022-10-23 16:07:21 +1100

    Integrate MaterialUI

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
+import theme from './theme'
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h2>Hello!</h2>
+    </ThemeProvider>
+  )
+}
 
 function render() {
   const container = document.getElementById('app')
   const root = createRoot(container)
-  root.render(<h2>Hello from React!</h2>)
+  root.render(<App />)
 }
 
 render()
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ccc',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
+})
+
+export default theme
